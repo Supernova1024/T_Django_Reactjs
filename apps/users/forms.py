9b29e75ae645b1f4e7681f8a84756207904b766f
Supernova1024 <novasuper042@gmail.com>
2022-08-08 06:18:35 -0700

    create

---
+++ apps/users/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserChangeForm,UserCreationForm
+
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserChangeForm):
+        model = User
+        fields =["email","username","first_name","last_name"]
+        error_class = "error"
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields =["email","username","first_name","last_name"]
+        error_class = "error"
+
---
+++ real_estate/settings/development.py
@@ -0,0 +1,21 @@
+from .base import *
+## postgresql
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
+
+
+## sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
